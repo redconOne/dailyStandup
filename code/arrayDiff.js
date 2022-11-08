758f56c0e0514f95b198f8d929d682fcf90eb3ec
redconOne <mingleeng@gmail.com>
2022-11-08 15:46:18 -0700

    add: arrayDiff.js

---
+++ code/arrayDiff.js
@@ -0,0 +1,12 @@
+// Implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+function arrayDiff(a, b) {
+  // your code here
+}
+
+module.exports = arrayDiff;
