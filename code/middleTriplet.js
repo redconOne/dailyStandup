42832b393080e3adac4a1f8cc733a9964407e73c
Ming Lee Ng <69211773+redconOne@users.noreply.github.com>
2022-11-14 12:50:07 -0700

    fix: added instructions

--- code/middleTriplet.js
+++ code/middleTriplet.js
@@ -1,3 +1,7 @@
+// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
 function middleTriplet(arr) {
   // your code here
 }
