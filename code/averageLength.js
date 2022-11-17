c6ea81b5e5c930ca60bb20f2e5184a300978b961
redconOne <mingleeng@gmail.com>
2022-11-16 18:52:36 -0700

    add: averageLength.js

---
+++ code/averageLength.js
@@ -0,0 +1,7 @@
+//Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+function averageLength(arr) {
+  // your code here
+}
+
+module.exports = averageLength;
