7025b1b776aa6506015834cca29e1aa97d35a9e6
redconOne <mingleeng@gmail.com>
2022-11-21 13:12:30 -0700

    add: removeConsecutiveDuplicates.js

---
+++ code/removeConsecutiveDuplicates.js
@@ -0,0 +1,7 @@
+//Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For Example: 'alpha beta beta gamma gamma gamma' -> 'alpha beta gamma'
+
+function removeConsecutiveDuplicates(str) {
+  // your code here
+}
+
+module.exports = removeConsecutiveDuplicates;
